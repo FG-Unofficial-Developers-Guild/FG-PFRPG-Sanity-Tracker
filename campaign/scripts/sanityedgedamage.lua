acef0e2d1479e67328b452364c7019e97dd72bb1
Wil T <wil.t.me@pm.me>
2025-03-07 20:28:28 -0500

    stylua

--- campaign/scripts/sanityedgedamage.lua
+++ campaign/scripts/sanityedgedamage.lua
@@ -8,12 +8,12 @@ end
 
 function onValueChanged()
 	local nodeSanity = DB.getParent(getDatabaseNode())
-	local nSanityDamage = DB.getValue(nodeSanity, "damage", 0)
-	local nSanityEdge = DB.getValue(nodeSanity, "edge", 0)
+	local nSanityDamage = DB.getValue(nodeSanity, 'damage', 0)
+	local nSanityEdge = DB.getValue(nodeSanity, 'edge', 0)
 
 	if nSanityDamage ~= 0 and nSanityDamage >= nSanityEdge then
-		window.sanitydamage.setColor(ColorManager.getUIColor("health_wounds_critical"))
+		window.sanitydamage.setColor(ColorManager.getUIColor('health_wounds_critical'))
 	else
-		window.sanitydamage.setColor(ColorManager.getUIColor("usage_full"))
+		window.sanitydamage.setColor(ColorManager.getUIColor('usage_full'))
 	end
 end
